324366c70a1afb80a9ffeaa2192214250f6eab86
HofuTsai15 <HofuTsai15@users.noreply.github.com>
2015-07-11 13:55:53 -0700

    Update StampDispenser.java

--- StampDispenser.java
+++ StampDispenser.java
@@ -6,7 +6,9 @@ import java.util.*;
  */
 public class StampDispenser
 {
+	// List for storing stamp values
     List<Integer> stamps = new ArrayList<Integer>();
+    // Set for eliminating duplicate stamp values
     Set<Integer> set = new HashSet<Integer>();
 
     /**
@@ -38,7 +40,7 @@ public class StampDispenser
         // Assign the values in the hashset back to the list
         stamps.addAll(set);
 
-        // Sort the array
+        // Sort the array in reverse order
         Collections.sort(stamps, Collections.reverseOrder());
     }
  
@@ -50,13 +52,70 @@ public class StampDispenser
      */
     public int calcMinNumStampsToFillRequest(int request)
     {  
-        return 0;
+    	// First check for special cases
+    	if (request < 0) return -1;
+    	else if (request == 0) return 0;
+    	// Implement a dynamic programming method
+    	else {
+    		return dynamicProgramming(request);
+    	}
+    }
+    
+    /**
+     * A helper function for calcMinNumStampsToFillRequest
+     * Returns the minimum number of stamps that the machine can dispense to
+     * fill the given request. Special cases of the request have been taken care of. 
+     *
+     * @param n The total value of the stamps to be dispensed.
+     */
+    
+    public int dynamicProgramming(int n) {
+    	int len = stamps.size();
+    	/* A array for dynamic programming: 
+    	 * Each element representing the minimum number of stamp used 
+    	 * or values of its index 
+         */
+    	int[] numStamp = new int[n+1];
+    	// Initialize the initial condition
+    	numStamp[0] = 0;
+    	
+    	/* For each value i, iterate through the stamp values
+    	 * If value i is larger or equal to current stamp value, stamps.get(j), 
+    	 * meaning value i can be filled by 1 + (the number of stamps needed for 
+    	 * value i-stamps.get(j)) number of stamps. 
+    	 * Then, compare with the current min value, and update min if necessary
+    	 */
+    	
+    	for (int i = 1; i <= n; i++) {
+    		int min = Integer.MAX_VALUE;
+    		for (int j = 0; j < len; j++) {
+    			if (i >= stamps.get(j)) {
+    				if (1 + numStamp[i-stamps.get(j)] < min) {
+    					min = 1 + numStamp[i-stamps.get(j)];
+    				}
+    			}
+    		}
+    		numStamp[i] = min;
+    	}
+    	return numStamp[n];
     }
     
     public static void main(String[] args) throws InputInvalidException
     {
         int[] denominations = { 30, 10, 24, 1, 6, 2, 90};
         StampDispenser stampDispenser = new StampDispenser(denominations);
-        //assert stampDispenser.calcMinNumStampsToFillRequest(18) == 3;
+
+        // Wrong input
+        assert(stampDispenser.calcMinNumStampsToFillRequest(-10) == -1);
+        assert(stampDispenser.calcMinNumStampsToFillRequest(-1) == -1);
+        assert(stampDispenser.calcMinNumStampsToFillRequest(0) == 0);
+
+        // Additional tests
+        assert(stampDispenser.calcMinNumStampsToFillRequest(1) == 1);
+        assert(stampDispenser.calcMinNumStampsToFillRequest(2) == 1);
+        assert(stampDispenser.calcMinNumStampsToFillRequest(5) == 3);
+        assert(stampDispenser.calcMinNumStampsToFillRequest(18) == 3);
+        assert(stampDispenser.calcMinNumStampsToFillRequest(34) == 2);
+        assert(stampDispenser.calcMinNumStampsToFillRequest(72) == 3);
     }
 }
